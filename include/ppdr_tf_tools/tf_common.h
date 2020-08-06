b88f6fc3eacba5a6ef535e5a462ed31ce11ce0a3
ppdr <osg273.w98@gmail.com>
2020-08-07 00:54:20 +0900

    change to wait until all tfs are available

--- include/ppdr_tf_tools/tf_common.h
+++ include/ppdr_tf_tools/tf_common.h
@@ -13,7 +13,8 @@ public:
     geometry_msgs::TransformStamped _tf_stamped;
 
     TFHandler(){};
-    geometry_msgs::TransformStamped get_tf(std::string parent_frame, std::string child_frame);
+    geometry_msgs::TransformStamped get_tf(std::string parent_frame, std::string child_frame, const float timeout = 1.0);
+    geometry_msgs::TransformStamped get_tf_immediate(std::string parent_frame, std::string child_frame);
     void send_tf(std::string parent_frame, std::string child_frame);
     void send_tf(geometry_msgs::TransformStamped tf_stamped, std::string parent_frame, std::string child_frame);
 
--- src/tf_common.cpp
+++ src/tf_common.cpp
@@ -2,14 +2,43 @@
 
 namespace ppdr_tf_tools{
 
-geometry_msgs::TransformStamped TFHandler::get_tf(std::string parent_frame, std::string child_frame){
+geometry_msgs::TransformStamped TFHandler::get_tf(const std::string parent_frame, const std::string child_frame, const float timeout){
+  // Wait until all tfs are available.
+  // All interpolation will be considered, however it takes a short time.
+  static tf2_ros::Buffer tfBuffer;
+  static tf2_ros::TransformListener tfListener(tfBuffer);
+  geometry_msgs::TransformStamped transformStamped;
+  transformStamped.transform.rotation.w = 1.0;
+
+  ros::Time now = ros::Time::now();
+  if(!tfBuffer.canTransform(parent_frame, child_frame, now, ros::Duration(timeout))){
+    ROS_WARN("Could not lookup transform from %s to %s, in duration %f [sec]", parent_frame.c_str(), child_frame.c_str(), timeout);
+    return transformStamped;
+  }
+
+  try{
+    transformStamped = tfBuffer.lookupTransform(parent_frame, child_frame, now, ros::Duration(0.1));
+  }
+  catch(tf2::TransformException &ex){
+    ROS_WARN("%s", ex.what());
+    return transformStamped;
+  }
+
+  _tf_stamped = transformStamped;
+
+  return transformStamped;
+}
+
+geometry_msgs::TransformStamped TFHandler::get_tf_immediate(std::string parent_frame, std::string child_frame){
+  // Get tf immediately.
+  // It takes no time, however some part of tf may be old.
   static tf2_ros::Buffer tfBuffer;
   static tf2_ros::TransformListener tfListener(tfBuffer);
   geometry_msgs::TransformStamped transformStamped;
   transformStamped.transform.rotation.w = 1.0;
 
   try{
-    transformStamped = tfBuffer.lookupTransform(parent_frame, child_frame, ros::Time(0), ros::Duration(5.0));
+    transformStamped = tfBuffer.lookupTransform(parent_frame, child_frame, ros::Time(0), ros::Duration(0.1));
   }
   catch(tf2::TransformException &ex){
     ROS_WARN("%s", ex.what());
